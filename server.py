e866b5116ee43716fa927003443abab8baf8deef
mupsy <amm@mupsy.net>
2025-02-26 15:32:09 +0800

    redirect default html to index.html

--- server.py
+++ server.py
@@ -19,20 +19,14 @@ for m in ENABLE:
     module = __import__("core.router.{}".format(m), globals(), locals(), ["router"], 0)
     app.include_router(module.router)
 
+from fastapi.responses import HTMLResponse
+@app.get("/", response_class = HTMLResponse)
+async def read_index():
+    with open("ui/dist/index.html", "rb") as f:
+        b = f.read()
+    return HTMLResponse(content = b)
 app.mount("/", StaticFiles(directory="ui/dist"), name = "static")
+
 if __name__ == "__main__":
-    import time
-    import threading
-
-    from core.router.sts import cm
-    from core.llm.chatgpt import chat
-    def timeHandler():
-        # 定时任务
-        while True:
-            cm.check_llm_message(chat)
-            time.sleep(30) # 30s 执行一次
-    thread = threading.Thread(target=timeHandler, daemon=True)
-    thread.start()
-
     import uvicorn
     uvicorn.run(app, host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", "8000")))
