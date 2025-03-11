0263cf66d30491b49a540416d73351f83d680c9f
mupsy <amm@mupsy.net>
2025-03-11 21:58:17 +0800

    update denoise frcrn

--- model/denoise.py
+++ model/denoise.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import numpy as np
 from model.frcrn import FRCRN
 model = FRCRN(
@@ -6,10 +7,12 @@ model = FRCRN(
     os.getenv("FRCRN_DEVICE", "cuda"),
 )
 
-def denoise(arr: np.ndarray, sr: int):
+def denoise(arr: np.ndarray | torch.Tensor, sr: int):
     """
     arr: 一维数组
     sr: 采样率
     """
+    if isinstance(arr, np.ndarray):
+        arr = torch.from_numpy(arr)
     return model.forward(arr, sr)
 
--- model/frcrn/__init__.py
+++ model/frcrn/__init__.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-import librosa
+import torchaudio
 from modelscope.models.base.base_model import Model
 from modelscope.models.audio.ans.frcrn import FRCRNDecorator
 from modelscope.utils.audio.audio_utils import audio_norm
@@ -13,20 +13,26 @@ class FRCRN():
         self.device = device
 
     @torch.no_grad()
-    def forward(self, x: np.ndarray, sr: int):
+    def forward(self, x: torch.FloatTensor, sr: int):
         """
         输入和输出应该都是numpy.ndarray! 待改
         x 应该是二维数组, 第一维是channel, 第二维是frame
         """
         if sr != 16000:
-            # 必须用librosa, 不然降噪出问题。绝了
-            x = librosa.resample(x, orig_sr=sr, target_sr=16000)
+            x = torchaudio.transforms.Resample(
+                orig_freq = sr,
+                new_freq = 16000,
+                resampling_method = "sinc_interp_kaiser",
+                lowpass_filter_width = 64,
+                rolloff = 0.9475937167399596,
+                beta = 14.769656459379492
+            )(x)
         x = audio_norm(x)
-        x = torch.from_numpy(x).to(self.device).unsqueeze(0)
+        x = x.unsqueeze(0).to(self.device)
         _, t = x.shape
+
         window = 16000
         stride = int(window * 0.75)
-
         # 是否需要分段
         decode_do_segement = False
         if t > window * 120:
@@ -42,7 +48,6 @@ class FRCRN():
             x = torch.nn.functional.pad(x, (0, t - (t - window) // stride * stride))
 
         if decode_do_segement:
-            noise = torch.zeros(t, device="cpu") # 仅存储, 没必要放显卡
             voice = torch.zeros(t, device="cpu") # 仅存储, 没必要放显卡
             give_up_length = (window - stride) // 2
             currend_idx = 0
@@ -51,22 +56,23 @@ class FRCRN():
                     noisy = x[:, currend_idx:currend_idx + window]
                 )
                 output = self.model(tmp_input)
-                tmp_noise = output['wav_l1'][0].cpu()
                 tmp_voice = output['wav_l2'][0].cpu()
                 if currend_idx == 0:
-                    noise[currend_idx:currend_idx + window - give_up_length] = tmp_noise[:-give_up_length]
                     voice[currend_idx:currend_idx + window - give_up_length] = tmp_voice[:-give_up_length]
                 else:
-                    noise[currend_idx + give_up_length:currend_idx + window - give_up_length] = tmp_noise[give_up_length:-give_up_length]
                     voice[currend_idx + give_up_length:currend_idx + window - give_up_length] = tmp_voice[give_up_length:-give_up_length]
                 currend_idx += stride
         else:
             output = self.model(dict(noisy = x))
-            noise = output['wav_l1'][0].cpu()
             voice = output['wav_l2'][0].cpu()
-        noise = noise[:t].numpy()
-        voice = voice[:t].numpy()
+        voice = voice[:t]
         if sr != 16000:
-            noise = librosa.resample(noise, orig_sr=16000, target_sr=sr)
-            voice = librosa.resample(voice, orig_sr=16000, target_sr=sr)
-        return noise * (2 ** 15), voice * (2 ** 15)
+            voice = torchaudio.transforms.Resample(
+                orig_freq = 16000,
+                new_freq = sr,
+                resampling_method = "sinc_interp_kaiser",
+                lowpass_filter_width = 64,
+                rolloff = 0.9475937167399596,
+                beta = 14.769656459379492
+            )(voice)
+        return voice * (2 ** 15)
