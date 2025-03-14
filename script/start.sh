debb637c1581ceb4089513821f003f0cab51e61c
mupsycho <amm@mupsy.net>
2025-03-14 07:23:44 +0000

    update docker script

--- script/start.sh
+++ script/start.sh
@@ -13,6 +13,8 @@ if [ ! -f "$FLAG_ENV_FILE" ]; then
     pip install --upgrade pip
     pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124 -t /app/env
     pip install -r requirements.txt -t /app/env
+    pip unstall numpy
+    pip install numpy==1.23.4 -t /app/env
 
     touch "$FLAG_ENV_FILE"
     echo "Dependencies installed."
